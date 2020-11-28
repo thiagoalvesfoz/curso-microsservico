538004e8b8317ca471bde4c2b1b815ee4773c749
Thiago Alves <thiago_marketingdigital@hotmail.com>
2020-11-28 19:27:50 -0300

    Feign client implementation

--- ms-payroll/src/main/java/com/course/mspayroll/services/PaymentService.java
+++ ms-payroll/src/main/java/com/course/mspayroll/services/PaymentService.java
@@ -1,33 +1,21 @@
 package com.course.mspayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.course.mspayroll.entities.Payment;
 import com.course.mspayroll.entities.Worker;
+import com.course.mspayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-	
-	@Value("${ms-worker.host}")
-	private String workerHost;
-	
+
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(long workerId, int days) {
 		
-		Map<String, String> uriVariables = new HashMap<>();		
-		uriVariables.put("id", ""+workerId);
-
-		Worker worker = restTemplate.getForObject(workerHost + "workers/{id}", 
-				Worker.class, uriVariables);
-				
+		Worker worker = workerFeignClient.findById(workerId).getBody();				
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
