b3cad7e6ad93950cfcd3203a5d3be494cd6c7e3e
Thiago Alves <thiago_marketingdigital@hotmail.com>
2020-11-28 18:04:07 -0300

    project ms-payroll created

---
+++ ms-payroll/src/test/java/com/course/mspayroll/MsPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.mspayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
